18c12dcefbf58199c4ae9d51025a26d24f30f7f5
RitishKarki <karki.ritish@gmail.com>
2020-02-27 17:36:43 -0500

    Third assignment

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,38 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing a list of landmarks.
+*/
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var userData: UserData
+    
+    var body: some View {
+        NavigationView {
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Show Favorites")
+            }
+            
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+        }
+            .navigationBarTitle(Text("List of landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(UserData())
+    }
+}
